c05201c30acbd3b7b396a8afc673cf2b5ca79f4c
tomliyouxiu <573435675@qq.com>
2023-10-31 23:16:24 +0800

    2023年10月31日23:16:14 go的变量

---
+++ Code/Day01/Basic.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+// 但是此时不可以使用:=定义变量
+// 如果需要定义多个变量时可以使用var()
+var (
+	aa = 33
+	ss = "kkk"
+)
+
+// go和其他的语言不太一样，进行初始化的时候会自动设置初值 int=0 string为空字符串
+func variableZeroValue() {
+	var a int
+	var s string
+	//Printf 类似于C语言的格式化输出
+	fmt.Printf("%d %q\n", a, s)
+}
+
+// go语言中定义了变量一定要用到要不然会出错
+func variableInitiateValue() {
+	var a, b int = 3, 4
+	var s string = "abc"
+	fmt.Println(a, b, s)
+}
+
+// 变量中的类型也可以省略掉 跟python类似 甚至可以在一行中定义不同的类型
+func variableTypeDeduction() {
+	var a, b, s, c = 3, 4, "abc", true
+	fmt.Println(a, b, s, c)
+}
+
+// 也可以省略var用：代替（在定义变量时，再次进行更改时不再需要：）
+func variableShrter() {
+	a, b, s, c := 3, 4, "abc", true
+	b = 5
+	fmt.Println(a, b, s, c)
+}
+
+func main() {
+	fmt.Println("Hello World")
+	variableZeroValue()
+	variableInitiateValue()
+	variableTypeDeduction()
+	variableShrter()
+	fmt.Println(aa, ss)
+}
